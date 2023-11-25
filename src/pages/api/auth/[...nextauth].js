8ac643cb58e2507646cf0532ec2293d07629d552
abdelfattah-el2 <abdelnasserwaled400@gmil.com>
2023-11-25 16:02:35 +0200

    after change Api Auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
+    
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
